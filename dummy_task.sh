1831af057330b6f56c1a52ccd9437484c15be3b5
ggungor7 <esragizemgungor@gmail.com>
2020-12-07 13:42:41 +0300

    Closes #8

--- dummy_task.sh
+++ dummy_task.sh
@@ -11,10 +11,3 @@ echo "";
 ./cli.py --auth ${name}:${pass} create "Dummy Task" --labels labels.json local car1.jpg car2.jpg car3.jpg car4.jpg car5.jpg car6.jpg car7.jpg car8.jpg
 
 ./cli.py --auth ${name}:${pass} upload --format "CVAT 1.1" 1 annotations.xml
-
-
-
-
-
-
-
